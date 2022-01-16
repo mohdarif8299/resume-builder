ee34014c95b05c5edac74f81d32d9791520d6016
Mohd Arif <arif@Mohds-MacBook-Air.local>
2022-01-17 01:21:53 +0530

    initial-commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,13 @@ class UsersController < ApplicationController
   def create_user
     user = User.find_by(email: params[:user][:email])
     if user.present?
-      flash.now[:danger] = 'Email already in use'
+      flash.now[:danger] = 'Email already in use.'
       render('home/signup') and return
     end
 
     user = User.create(create_params(params))
     Profile.create(user: user)
-    flash.now[:success] = 'Account Created Successfully'
+    flash.now[:success] = 'Account Created Successfully.'
     render('sessions/new')
   end
 end
